0bb4ce7d45c0b82a73f268eebe8ccc5a697844bb
Brent Rodriguez <brent.s.rodriguez@gmail.com>
2020-10-26 21:02:46 -0500

    Added Server

---
+++ server-express-mongodb/server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+
+//Initiate Express App
+const app = express()
+
+
+//DB Connection
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
+.then(() => console.log('Connected to DB ...'))
+.catch(err => console.log('DB Connection Error: ', err))
+
+//Routes Import
+const authRoutes = require('./routes/auth')
+
+//Application Middleware
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+//app.use(cors()) // ALLOWS ALL ORIGINS
+if(process.env.NODE_ENV = 'development'){
+  app.use(cors({origin:`http://localhost:3000`}))
+}
+
+//Middleware
+app.use('/api', authRoutes)
+
+
+const port = process.env.PORT || 8000
+app.listen(port, () => {
+  console.log(`Team Spongebob says - Server is running on port ${port} ...`)
+})
