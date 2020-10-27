17887b6a88d7a92bc5c816caf458ceb295146252
Brent Rodriguez <brent.s.rodriguez@gmail.com>
2020-10-26 22:17:40 -0500

    Asdded Validators/auth

---
+++ server-express-mongodb/validators/auth.js
@@ -0,0 +1,21 @@
+const { check } = require('express-validator')
+
+
+exports.userRegistrationValidator = [
+    //Validate Name
+    check('name')
+    .not()
+    .isEmpty()
+    .withMessage('Name is required'),
+
+    //Validate Email
+    check('email')
+    .not()
+    .isEmpty()
+    .withMessage('Email is required'),
+
+    //Validate Password
+    check('password')
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters long')
+]
