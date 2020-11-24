9006ce9402e2b02957db1d2641f1285a33d13f82
Camila <camila.martinezsalellas@my.scitexas.com>
2020-11-24 03:04:47 -0500

    Updates in closet dashboard

--- client/src/components/MyClosetCards.js
+++ client/src/components/MyClosetCards.js
@@ -2,25 +2,16 @@ import React from 'react';
 import './MyClosetCollection.css';
 
 
-// const MyClosetCard = props => {
-//   return (
-//     <div className="card">
-//       <div className="card-body">
-//         <h5 className="card-title">{props.type}</h5>
-//         <p className="card-text">{props.numItems}</p>
-//       </div>
-//     </div>
-//   )
-// }
-
 const MyClosetCard = props => {
   return (
     <div className="card border-light" >
+      <div className="overflow">
         <img src={props.imgsrc} alt="T-shirts" className="card-img rounded" />
         <div className="card-body d-inline-block text-justify text-white">
         <div className="card-img-overlay">
           <h5 className="card-title font-weight-bold">{props.type}</h5>
           <p className="card-text">{props.numItems}</p>
+        </div>
         </div>
       </div>
     </div>
